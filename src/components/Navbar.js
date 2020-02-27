c3fc057e1986676d47ed2cb541aa3882fdf7ad7a
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2020-02-26 19:10:48 -0600

    Simple menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,18 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import "./navbar.css";
+import logo from "../img/3ED.gif";
+import SimpleMenu from "./SimpleMenu";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
 			<div>
-				<AppBar position="fixed">
-					<Toolbar>
-						<Typography variant="title" color="inherit">
-							React & Material-ui (really simple) sample page
-						</Typography>
+				<AppBar id="navbar" position="fixed">
+					<SimpleMenu />
+					<Toolbar id="toolbar">
+						<img src={logo} />
 					</Toolbar>
 				</AppBar>
 			</div>
